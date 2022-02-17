b68b4ffcbb89a47e990ff4dfb77dda31a5c63a4b
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2022-02-17 20:42:53 +0530

    Day1 17 Feb 2022

---
+++ DSA/Array/Pairs_TwoSum.java
@@ -0,0 +1,3 @@
+public class Pairs_TwoSum {
+    
+}
---
+++ DSA/Array/missingNumber.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class missingNumber{
+    public static void main(String[] args){
+        int arr[]={1,2,3,4,5,6,8,9,10};
+        /*for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i] +"="+(i+1));
+            if(arr[i]!=i+1) {
+                System.out.println("Missing number= "+(i+1));
+                break;
+            }
+        }*/
+        int n=arr.length;
+        int actualLength=n+1;
+        int actualSum=actualLength*(actualLength+1)/2;
+        System.out.println(actualSum);
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+        System.out.println("Missing Number= "+(actualSum-sum));
+    }
+}
