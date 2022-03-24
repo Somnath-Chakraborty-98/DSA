7659a3152dab9ac84a5fb0f181b3104d34662b07
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2022-03-24 20:25:18 +0530

    24th, March, 2022

--- DSA/LinkedList/doublyLinkedList.java
+++ DSA/LinkedList/doublyLinkedList.java
@@ -1,16 +1,22 @@
-public class LinkedList {
+public class doublyLinkedList {
+    
     Node head;
 
     static class Node{
         int data;
+        Node prev;
         Node next;
 
         Node(int data){
             this.data=data;
             next=null;
+            prev=null;
         }
     }
-//Node Creation Complete
+
+
+
+
 
     boolean isEmpty(){
         return head == null;
@@ -19,47 +25,48 @@ public class LinkedList {
     void display (){
         Node temp = head;
 
+        System.out.print(" START");
         while(temp!=null){
-            System.out.print(temp.data+" -> ");
+            System.out.print(" <- "+temp.data+" -> ");
             temp=temp.next;
         }
         System.out.println("END");
     }
 
     void addAtStart(int data){
-
-        Node node = new Node(data);
+        Node node= new Node(data);
         node.next=head;
-        head = node; 
+        node.prev=null;
+        head=node;
     }
 
     void addToEnd(int data){
         Node node = new Node(data);
         Node temp = head;
 
-        if(isEmpty()) {
+        if(isEmpty()){
             addAtStart(data);
             return;
-        } 
+        }
 
         while(temp.next!=null){
             temp=temp.next;
         }
-
         temp.next=node;
+        node.prev=temp;
         node.next=null;
     }
 
     void addAt(int index, int data){
+        Node node = new Node(data);
         Node temp = head;
-        Node node= new Node(data);
 
-        if(isEmpty() || index == 0){
+        if(isEmpty() || index==0){
             addAtStart(data);
             return;
         }
 
-        for(int i=0;i< index-1;i++){
+        for(int i=0;i<index-1;i++){
 
             
 
@@ -67,19 +74,24 @@ public class LinkedList {
                 addToEnd(data);
                 return;
             }
-
             temp=temp.next;
         }
+
         node.next=temp.next;
+        temp.next.prev=node;
         temp.next=node;
+        node.prev=temp;
+
     }
-    
+
     void deleteFirst(){
         if(isEmpty()) return;
         Node temp = head;
-        head=temp.next;
-        temp.next=null;
         System.out.println("deleted = " + temp.data);
+
+        head=temp.next;
+        head.prev=null;
+        temp.next=null;
     }
 
     void deleteLast(){
@@ -90,20 +102,19 @@ public class LinkedList {
             temp=temp.next;
         }
         System.out.println("deleted = " + temp.next.data);
+        temp.next.prev=null;
         temp.next=null;
     }
 
     void deleteAt(int index){
         if(isEmpty()) return;
         Node temp = head;
-
         if(index == 0){
             deleteFirst();
             return;
         }
 
-        for(int i=0;i< index-1;i++){
-
+        for(int i=0;i<index-1;i++){
             
 
             if(temp.next.next==null){
@@ -114,24 +125,30 @@ public class LinkedList {
             temp=temp.next;
         }
         System.out.println("deleted = " + temp.next.data);
+        temp.next.next.prev=temp;
         temp.next=temp.next.next;
-
     }
 
-    public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
 
-        ll.addAtStart(5);
-        ll.addAtStart(6);
-        ll.addToEnd(10);
+
+
+    public static void main(String[] args) {
+        doublyLinkedList ll = new doublyLinkedList();
+
+        
         ll.addAtStart(1);
-        ll.addToEnd(11);
-        ll.addAt(3, 7);
+        ll.addToEnd(10);
+        ll.addAt(1, 3);
+        ll.addAt(2, 4);
+        ll.addAt(1, 2);
+        ll.addAt(1, 11);
+        ll.addAt(0, 0);
         ll.deleteFirst();
-        ll.addAtStart(15);
-        ll.addAtStart(4);
         ll.deleteLast();
-        ll.deleteAt(5);
+        ll.deleteAt(1);
+        System.out.println(ll.head.data);
+        
+        
 
 
         ll.display();
