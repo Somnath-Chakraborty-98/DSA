2d2ced714dff126dc955f8f678e214cdd8ca7b25
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2023-08-15 16:06:29 +0530

    mountain array

---
+++ Problem Solving/Algorithms/Searching/BS Questions/MountainArray.java
@@ -0,0 +1,26 @@
+public class MountainArray {
+    
+    static int solution(int[] arr){
+        int start = 0, mid = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            mid = start + (end - start) / 2;
+
+            if (arr[mid+1] < arr[mid])  // you are in decreasing part of array, it maybe the ans
+                end = mid;              // this is why end != mid - 1
+            else                        // you are in ascending part of the array
+                start = mid + 1;        // because we kniow that mid + 1 element > mid, hence we ignore mid
+        }
+
+        // in the end start == end and pointing to largest niumber because of two checks above
+        // start and end are always trying to find max element
+        return start;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,4,7,5,2};
+        System.out.println(MountainArray.solution(arr));
+    }
+}
