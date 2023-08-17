eef7a0ff2c0fabfccfbb61d59516abe906afe8c8
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2023-08-17 07:14:00 +0530

    count of rotation of an array

---
+++ Problem Solving/Algorithms/Searching/BS Questions/CountOfRotationOfArray.java
@@ -0,0 +1,36 @@
+public class CountOfRotationOfArray {
+
+    static int solution(int[] arr) {
+
+        int pivot = findPivot(arr);
+        return pivot + 1;
+
+    }
+
+    static int findPivot(int[] arr) {
+
+        int start = 0, mid = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+
+            if (mid < end && arr[mid + 1] < arr[mid])
+                return mid;
+            if (mid > start && arr[mid - 1] > arr[mid])
+                return mid - 1;
+            if (arr[mid] <= arr[start])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        System.out.println(CountOfRotationOfArray.solution(arr));
+    }
+}
