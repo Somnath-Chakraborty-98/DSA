2f5c7cc95330d566ff03c2b12e4006fa54c90de5
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2023-09-07 07:35:24 +0530

    Selection Sort

--- Problem Solving/Algorithms/Sorting/Bubble Sort/BubbleSort.java
+++ Problem Solving/Algorithms/Sorting/Bubble Sort/BubbleSort.java
@@ -1,26 +1,27 @@
 import java.util.Arrays;
 
-public class BubbleSort{
+public class BubbleSort {
 
-    static void sort(int[] arr){
+    static void sort(int[] arr) {
         boolean swapped = false;
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             swapped = false;
-            for(int j = 1; j< arr.length - i; j++){
+            for (int j = 1; j < arr.length - i; j++) {
 
-                if(arr[j] < arr[j-1]){
+                if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                     swapped = true;
                 }
             }
-            if(!swapped) break;
+            if (!swapped)
+                break;
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {5,7,2,1,9,6};
+        int[] arr = { 5, 3, 4, 2, 6, 1 };
         sort(arr);
         System.out.println(Arrays.toString(arr));
     }
---
+++ Problem Solving/Algorithms/Sorting/Bubble Sort/SelectionSort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    static void sort(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 3, 7, 1, 9 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
