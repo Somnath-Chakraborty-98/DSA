64ec99926e67ad4b630080f030ebd4f5bc8c5efe
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2023-09-29 07:29:56 +0530

    Recursion - 1 to N

--- Problem Solving/Recursion/OneToN.java
+++ Problem Solving/Recursion/OneToN.java
@@ -1,3 +1,15 @@
 public class OneToN {
-    
+
+    static void solution(int n) {
+
+        if (n == 0)
+            return;
+
+        solution(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        solution(5);
+    }
 }
