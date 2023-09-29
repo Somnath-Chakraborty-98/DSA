d6219e28f96593617d03cd4b64db661be7f41c0f
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2023-09-29 06:54:34 +0530

    Recursion - Binary Search

---
+++ Problem Solving/Recursion/BinarySearch.java
@@ -0,0 +1,22 @@
+public class BinarySearch {
+
+    static int search(int[] arr, int target, int s, int e) {
+        if (s > e)
+            return -1;
+
+        int m = s + (e - s) / 2;
+
+        if (arr[m] == target)
+            return m;
+
+        if (target < arr[m])
+            return search(arr, target, s, m - 1);
+
+        return search(arr, target, m + 1, e);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 45, 56, 78 };
+        System.out.println(search(arr, 90, 0, arr.length - 1));
+    }
+}
