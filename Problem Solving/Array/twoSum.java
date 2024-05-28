be3b391c2662d378e804de975e7bea08efa2bc90
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2024-05-28 06:40:04 +0530

    random

--- Problem Solving/Array/twoSum.java
+++ Problem Solving/Array/twoSum.java
@@ -1,31 +1,31 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class Solution { //Hashing
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        for(int i=0;i<nums.length;i++){
-            for(int j = i+1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target) return new int[] {i,j};
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
             }
         }
-        return new int[] {-1,-1};
-        
+        return new int[] { -1, -1 };
+
     }
 }
 
-
-class Solution2 {
+class Solution2 { // Hashing
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map= new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-    
-            if(map.containsKey(target - nums[i])){
-                return new int[] {map.get(target - nums[i]), i};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(target - nums[i])) {
+                return new int[] { map.get(target - nums[i]), i };
             }
             map.put(nums[i], i);
         }
-        return new int[] {-1,-1};
-        
+        return new int[] { -1, -1 };
+
     }
 }
