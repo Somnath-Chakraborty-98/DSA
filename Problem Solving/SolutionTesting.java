2e78c15c9c432e4ecedfb183994c246a0cf02c2b
Somnath-Chakraborty-98 <71351098+Somnath-Chakraborty-98@users.noreply.github.com>
2024-06-12 07:39:22 +0530

    Revision

---
+++ Problem Solving/SolutionTesting.java
@@ -0,0 +1,22 @@
+
+public class SolutionTesting {
+
+    void ans(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        SolutionTesting st = new SolutionTesting();
+
+        int arr[] = { 4, 3, 2, 7, 8, 2, 3, 1 };
+
+        st.ans(arr);
+
+        // for(int i=0;i<arr.length;i++){
+        // if(arr[i] != i+1){
+        // System.out.println(arr[i]);
+        // }
+        // }
+    }
+
+}
